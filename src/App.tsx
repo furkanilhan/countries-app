dccae5bf627a8892242f3a077f7fd9c44474de4f
FURKAN ILHAN <evatro.ilhan@mercedes-benz.com>
2024-06-16 01:23:40 +0300

    ant design dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { ConfigProvider, theme, Button, Card } from "antd";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { CountryDetailComponent } from "./components/CountryDetailComponent/CountryDetailComponent";
@@ -5,13 +7,26 @@ import { CountryDetailComponent } from "./components/CountryDetailComponent/Coun
 import { HeaderComponent } from "./components/HeaderComponent/HeaderComponent";
 import { HomeComponent } from "./components/HomeComponent/HomeComponent";
 export const App = () => {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+  };
+
   return (
     <>
-      <HeaderComponent />
-      <Routes>
-        <Route path="/" element={<HomeComponent />} />
-        <Route path="/country/:countryName" element={<CountryDetailComponent />} />
-      </Routes>
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+        }}
+      >
+        <HeaderComponent />
+        <Button onClick={handleClick}>Change Theme to {isDarkMode ? "Light" : "Dark"}</Button>
+        <Routes>
+          <Route path="/" element={<HomeComponent />} />
+          <Route path="/country/:countryName" element={<CountryDetailComponent />} />
+        </Routes>
+      </ConfigProvider>
     </>
   );
 };
